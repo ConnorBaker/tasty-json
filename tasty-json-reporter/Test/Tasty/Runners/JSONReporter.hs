b4e03effe1a969944d66e7e7f1e0ee632146ca77
Connor Baker <connorbaker01@gmail.com>
2022-09-05 21:19:23 -0400

    tasty-json-reporter: Split testnames into group and name

--- tasty-json-reporter/Test/Tasty/Runners/JSONReporter.hs
+++ tasty-json-reporter/Test/Tasty/Runners/JSONReporter.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Test.Tasty.Runners.JSONReporter
@@ -13,9 +13,11 @@ where
 
 import qualified Control.Concurrent.STM as STM
 import Control.Monad (foldM)
-import Data.Aeson (ToJSON (toEncoding), fromEncoding)
+import Data.Aeson (ToJSON (toEncoding), encodeFile, fromEncoding)
 import Data.ByteString.Builder (hPutBuilder)
 import qualified Data.IntMap.Strict as IM
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NonEmpty
 import Data.Proxy (Proxy (Proxy))
 import System.IO (IOMode (WriteMode), withBinaryFile)
 import Test.Tasty (TestName, TestTree)
@@ -40,13 +42,22 @@ awaitResult statusMap i = STM.atomically $
         _ -> STM.retry
 
 -- Produces a list of list of tests names, where each list starts with the outermost test group and proceeds inwards, to the innermost test.
-fullTestNames :: OptionSet -> TestTree -> [[TestName]]
-fullTestNames = foldTestTree trivialFold {foldSingle = fs, foldGroup = fg}
+fullTestNames :: OptionSet -> TestTree -> [(NonEmpty TestName, TestName)]
+fullTestNames optionSet testTree = map splitNames testPath
   where
     fg :: OptionSet -> TestName -> [[TestName]] -> [[TestName]]
     fg _ groupName = map (groupName :)
     fs :: IsTest t => OptionSet -> TestName -> t -> [[TestName]]
     fs _ testName _ = [[testName]]
+
+    testPath :: [[TestName]]
+    testPath = foldTestTree trivialFold {foldSingle = fs, foldGroup = fg} optionSet testTree
+
+    splitNames :: [TestName] -> (NonEmpty TestName, TestName)
+    splitNames = \case
+      [] -> error "Empty list of test names"
+      [name] -> error $ "Test " ++ name ++ " has no parent group"
+      names -> ((NonEmpty.fromList . init) names, last names)
 
 jsonReporter :: Ingredient
 jsonReporter = TestReporter [Option $ Proxy @(Maybe JSONPath), Option $ Proxy @(Maybe JSONMeta)] $ \options tree -> do
@@ -57,27 +68,23 @@ jsonReporter = TestReporter [Option $ Proxy @(Maybe JSONPath), Option $ Proxy @(
         numThreads = getNumThreads $ lookupOption options
 
         -- The test number in the status map corresponds to the list of test group names to locate that test.
-        tests :: [(Int, [TestName])]
+        tests :: [(Int, (NonEmpty TestName, TestName))]
         tests = zip [0 ..] $ fullTestNames options tree
 
         testCount :: Int
         testCount = length tests
 
-        go :: (Bool, [JSONTestResult]) -> (Int, [TestName]) -> IO (Bool, [JSONTestResult])
-        go (accSuccess, accResults) (idx, fullTestName) = do
+        go :: (Bool, [JSONTestResult]) -> (Int, (NonEmpty TestName, TestName)) -> IO (Bool, [JSONTestResult])
+        go (accSuccess, accResults) (idx, (group, name)) = do
           result <- awaitResult statusMap idx
           let !success = resultSuccessful result && accSuccess
-          let !results = JSONTestResult fullTestName (fromResult result) : accResults
+          let !results = JSONTestResult group name (fromResult result) : accResults
           pure (success, results)
     (success, results) <- foldM go (True, []) tests
 
     return $ \time ->
       success <$ do
-        withBinaryFile path WriteMode $ \handle ->
-          hPutBuilder handle $
-            fromEncoding $
-              toEncoding $
-                JSONTestSuiteResult {..}
+        encodeFile path $ JSONTestSuiteResult {results, time, success, meta, numThreads, testCount}
 
 consoleAndJsonReporter :: Ingredient
 consoleAndJsonReporter = composeReporters consoleTestReporter jsonReporter
--- tasty-json-reporter/Test/Tasty/Types/JSONTestResult.hs
+++ tasty-json-reporter/Test/Tasty/Types/JSONTestResult.hs
@@ -3,10 +3,11 @@
 module Test.Tasty.Types.JSONTestResult where
 
 import Data.Aeson.TH (deriveJSON)
+import Data.List.NonEmpty (NonEmpty)
 import Test.Tasty (TestName)
 import Test.Tasty.Aeson.Options (tastyJSONOptions)
 import Test.Tasty.Types.JSONResult (JSONResult)
 
-data JSONTestResult = JSONTestResult {path :: [TestName], result :: JSONResult} deriving (Eq, Show)
+data JSONTestResult = JSONTestResult {group :: NonEmpty TestName, name :: TestName, result :: JSONResult} deriving (Eq, Show)
 
 $(deriveJSON tastyJSONOptions ''JSONTestResult)
