37e17dd76ba5f062b7080296aaa2656944f24048
Connor Baker <connorbaker01@gmail.com>
2022-09-10 17:23:21 -0400

    tasty-json-markdown: Add type for markdown tables

--- tasty-json-markdown/Main.hs
+++ tasty-json-markdown/Main.hs
@@ -1,20 +1,59 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
-import Data.Aeson (ToJSON (toJSON), eitherDecodeFileStrict')
-import System.Environment (getArgs)
-import Test.Tasty.Types.JSONTestSuiteResult (JSONTestSuiteResult (..))
+import Data.Aeson (eitherDecodeFileStrict')
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HashMap
+-- import Data.HashSet (HashSet)
+-- import qualified Data.HashSet as HashSet
 
-defaultMain :: String -> IO ()
+import Data.List.NonEmpty (NonEmpty)
+import System.Environment (getArgs)
+import Test.Tasty (TestName)
+import Test.Tasty.Types.JSONResult ()
+import qualified Test.Tasty.Types.JSONResult as JSONResult
+import qualified Test.Tasty.Types.JSONTestPath as JSONTestPath
+import Test.Tasty.Types.JSONTestResult (JSONTestResult)
+import qualified Test.Tasty.Types.JSONTestResult as JSONTestResult
+import Test.Tasty.Types.JSONTestSuiteResult (JSONTestSuiteResult)
+import qualified Test.Tasty.Types.JSONTestSuiteResult as JSONTestSuiteResult
+
+totalTestTime :: JSONTestSuiteResult -> Double
+totalTestTime =
+  sum
+    . fmap (JSONResult.durationInSeconds . JSONTestResult.result)
+    . JSONTestSuiteResult.results
+
+totalNumTests :: JSONTestSuiteResult -> Int
+totalNumTests = length . JSONTestSuiteResult.results
+
+groupByTestGroupPrefix :: [JSONTestResult] -> HashMap (NonEmpty TestName) [JSONTestResult]
+groupByTestGroupPrefix =
+  HashMap.fromListWith (<>)
+    . fmap (\jtr -> (jtr.path.group, pure jtr))
+
+-- groupedToTable :: HashMap [String] [JSONTestResult] -> Table
+-- groupedToTable grouped = HashMap.foldMapWithKey (k -> v -> m) (HashMap k v)
+--   where
+--     stripGroupPrefix :: [String] -> [JSONTestResult] -> [JSONTestResult]
+--     stripGroupPrefix prefix = fmap (\t -> t {JSONTestResult.path = drop (length prefix) (JSONTestResult.path t)})
+
+defaultMain :: FilePath -> IO JSONTestSuiteResult
 defaultMain jsonPath =
-  (eitherDecodeFileStrict' jsonPath) >>= \case
-    Left err -> putStrLn err
-    Right (json :: JSONTestSuiteResult) -> print $ toJSON json
+  eitherDecodeFileStrict' jsonPath >>= \case
+    Left err -> error err
+    Right (json :: JSONTestSuiteResult) -> pure json
 
 main :: IO ()
 main =
   getArgs >>= \case
-    [jsonPath] -> defaultMain jsonPath
+    [jsonPath] -> do
+      jtsr <- defaultMain jsonPath
+      putStrLn $ "Total test time: " <> show (totalTestTime jtsr)
+      putStrLn $ "Total number of tests: " <> show (totalNumTests jtsr)
+      putStrLn $ "Tests grouped by test group prefix:" <> show (groupByTestGroupPrefix $ JSONTestSuiteResult.results jtsr)
     _ -> putStrLn "Usage: tasty-json-markdown <json-path>"
---
+++ tasty-json-markdown/MarkdownTable.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+
+module MarkdownTable where
+
+import Data.Bool (bool)
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NonEmpty
+import Numeric (showFFloat)
+import Test.Tasty.Types.JSONTestPath (JSONTestPath)
+
+newtype MarkdownTable = MarkdownTable (NonEmpty MarkdownTableRow)
+
+data MarkdownTableRow = MarkdownTableRow
+  { path :: JSONTestPath,
+    success :: Bool,
+    durationInSeconds :: Double
+  }
+  deriving (Eq)
+
+succeeded :: Bool -> String
+succeeded = bool "❌" "✅"
+
+roundFloat :: RealFloat a => a -> String
+roundFloat = flip (showFFloat (Just 6)) ""
+
+instance Show MarkdownTableRow where
+  show :: MarkdownTableRow -> String
+  show mtr =
+    "| " ++ show mtr.path ++ " | " ++ succeeded mtr.success ++ " | " ++ roundFloat mtr.durationInSeconds ++ " |"
+
+instance Show MarkdownTable where
+  show :: MarkdownTable -> String
+  show (MarkdownTable rows) =
+    let header = "| Path | Success | Duration (s) |"
+        separator = "| --- | --- | --- |"
+     in unlines $ header : separator : map show (NonEmpty.toList rows)
