4763d0464ac74fdedf29b8989ad42cc03dbd1c3f
Connor Baker <connorbaker01@gmail.com>
2022-09-16 01:01:00 -0400

    Move tasty-json-reporter to hix

--- flake.nix
+++ flake.nix
@@ -14,59 +14,81 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    haskell-language-server = {
-      url = "github:haskell/haskell-language-server/aad896cdc5b4869dc781e9b1eade8f3b14148f7c";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    alejandra = {
-      url = "github:kamadorueda/alejandra";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    
+    hix.url = "github:tek/hix";
+    hix.inputs.nixpkgs.follows = "nixpkgs";
+    
+    haskell-language-server.url = "github:haskell/haskell-language-server/5d35a740b2fd70b225aaa94541ff22c3ed3e060a";
+    haskell-language-server.inputs.nixpkgs.follows = "nixpkgs";
+    
+    alejandra.url = "github:kamadorueda/alejandra";
+    alejandra.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-    alejandra,
+    hix,
     haskell-language-server,
+    alejandra,
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = import ./nix/overlays.nix;
-
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [overlays.default];
-      };
-
-      inherit (pkgs.haskell.packages.ghc924) shellFor tasty-json-reporter tasty-json-markdown ormolu hlint cabal-install cabal2nix;
-      inherit (pkgs.haskell.lib.compose) dontCheck dontHaddock;
-      inherit (pkgs.lib) flip pipe;
-      hls = haskell-language-server.packages.${system}.haskell-language-server-924;
-    in {
-      inherit overlays;
-      formatter = alejandra.packages.${system}.default;
-
-      apps = {
-        update-flake = {
-          type = "app";
-          program = ./nix/scripts/update-flake.sh;
-        };
-        update-cabal = {
-          type = "app";
-          program = ./nix/scripts/update-cabal.sh;
-        };
-      };
-
+    hix.lib.auto ({lib, ...}: {
+      output.systems = [ "aarch64-darwin" ];
+      compat.enable = true;
+      shell.hls.package = haskell-language-server.packages.aarch64-darwin.haskell-language-server-924;
       packages = {
-        inherit tasty-json-reporter tasty-json-markdown;
-        default = tasty-json-reporter;
-      };
-
-      devShells.default = shellFor {
-        packages = ps: with ps; [tasty-json-reporter tasty-json-markdown];
-        buildInputs = map (flip pipe [dontCheck dontHaddock]) [ormolu hlint cabal-install cabal2nix hls];
+        tasty-json-reporter = {
+          src = ./tasty-json-reporter;
+          dependencies = [
+            "aeson"
+            "base"
+            "bytestring"
+            "containers"
+            "stm"
+            "tagged"
+            "tasty"
+            "text"
+            "unordered-containers"
+          ];
+        };
       };
     });
+    
+    # flake-utils.lib.eachDefaultSystem (system: let
+    #   overlays = import ./nix/overlays.nix;
+
+    #   pkgs = import nixpkgs {
+    #     inherit system;
+    #     overlays = [overlays.default];
+    #   };
+
+    #   inherit (pkgs.haskell.packages.ghc924) shellFor tasty-json-reporter tasty-json-markdown ormolu hlint cabal-install cabal2nix;
+    #   inherit (pkgs.haskell.lib.compose) dontCheck dontHaddock;
+    #   inherit (pkgs.lib) flip pipe;
+    #   hls = haskell-language-server.packages.${system}.haskell-language-server-924;
+    # in {
+    #   inherit overlays;
+    #   formatter = alejandra.packages.${system}.default;
+
+    #   apps = {
+    #     update-flake = {
+    #       type = "app";
+    #       program = ./nix/scripts/update-flake.sh;
+    #     };
+    #     update-cabal = {
+    #       type = "app";
+    #       program = ./nix/scripts/update-cabal.sh;
+    #     };
+    #   };
+
+    #   packages = {
+    #     inherit tasty-json-reporter tasty-json-markdown;
+    #     default = tasty-json-reporter;
+    #   };
+
+    #   devShells.default = shellFor {
+    #     packages = ps: with ps; [tasty-json-reporter tasty-json-markdown];
+    #     buildInputs = map (flip pipe [dontCheck dontHaddock]) [ormolu hlint cabal-install cabal2nix hls];
+    #   };
+    # });
 }
--- nix/overlays.nix
+++ nix/overlays.nix
@@ -2,6 +2,16 @@
   default = final: prev: let
     inherit (prev.lib) pipe recursiveUpdateUntil;
     inherit (prev.haskell.lib.compose) dontCheck dontHaddock;
+    ghc902 = prev.haskell.packages.ghc902.extend (hself: hsuper: {
+      tasty-json-reporter = pipe (hsuper.callPackage ./packages/tasty-json-reporter.nix {}) [
+        dontCheck
+        dontHaddock
+      ];
+      tasty-json-markdown = pipe (hsuper.callPackage ./packages/tasty-json-markdown.nix {}) [
+        dontCheck
+        dontHaddock
+      ];
+    });
     ghc924 = prev.haskell.packages.ghc924.extend (hself: hsuper: {
       tasty-json-reporter = pipe (hsuper.callPackage ./packages/tasty-json-reporter.nix {}) [
         dontCheck
@@ -12,8 +22,8 @@
         dontHaddock
       ];
     });
-    new = {
-      haskell.packages.ghc924 = ghc924;
+    new.haskell.packages = {
+      inherit ghc902 ghc924;
     };
   in
     recursiveUpdateUntil (path: _: _: path == ["haskell" "packages" "ghc924"]) prev new;
