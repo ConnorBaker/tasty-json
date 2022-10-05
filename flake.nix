1f8e862c37b6649982b186f441d472d0f907c9d7
Connor Baker <connorbaker01@gmail.com>
2022-10-05 19:23:55 +0200

    Add cabal files; run on more systems

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
   }:
     hix.lib.flake ({ 
       base = ./.;
-      output.systems = ["aarch64-darwin"]; 
+      output.systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
       devGhc.compiler = "ghc902";
       main = "tasty-json-reporter";
       packages.tasty-json-reporter = ./tasty-json-reporter;
